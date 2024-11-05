afb3b31f4c7b015c2a13c2fadd9d73142f3ff148
Michael McClennen <mmcclenn@geology.wisc.edu>
2024-11-04 19:41:40 -0600

    The 'wing user' command now works (sort of).

--- lib/Wing/Command/Command/user.pm
+++ lib/Wing/Command/Command/user.pm
@@ -25,11 +25,13 @@ sub opt_spec {
       [ 'search=s', 'search users by keyword' ],
       [ 'list', 'list all users' ],
       [ 'list_admins', 'list all admin users' ],
+      [ 'name=s', 'the real name of the user' ],	    
       [ 'email=s', 'the email address for the user' ],
       [ 'password=s', 'the password for the user' ],
       [ 'username=s', 'a new username for the user' ],
       [ 'role=s', 'a new role for the user' ],
       [ 'status=s', 'a new status for the user' ],
+      [ 'person_no=s', 'a new person_no value for the user' ],
       [ 'admin!', 'whether the user should be an admin' ],
     );
 }
@@ -54,8 +56,9 @@ sub list_users {
 }
 
 sub add_user {
-    my ($users, $username, $password, $admin, $email, $role) = @_;
+    my ($users, $username, $password, $admin, $email, $role, $name) = @_;
     my $user = $users->new({});
+    set_name($user, $name);
     $user->username($username);
     $user->admin($admin) if $admin;
     $user->email($email);
@@ -65,29 +68,70 @@ sub add_user {
 }
 
 sub modify_user {
-    my ($users, $old_username, $new_username, $password, $admin, $email, $role, $status) = @_;
+    my ($users, $old_username, $new_username, $password, $admin, $email, $role, 
+	$status, $name) = @_;
     my $user = $users->search({username => $old_username}, { rows => 1})->single;
     ouch(440, $old_username.' not found.') unless defined $user;
-    $user->encrypt_and_set_password($password) if defined $password;
+    set_name($user, $name) if $name;
+    $user->encrypt_and_set_password($password) if $password;
     $user->admin($admin) if defined $admin;
-    $user->username($new_username) if defined $new_username;
-    $user->email($email) if defined $email;
-    $user->role($role) if defined $role;
-    $user->contributor_status($status) if defined $status;
+    $user->username($new_username) if $new_username;
+    $user->email($email) if $email;
+    $user->role($role) if $role;
+    $user->contributor_status($status) if $status;
+    # $user->person_no($person_no) if $person_no;
     $user->update;
+}
+
+sub set_name {
+    
+    my ($user, $name) = @_;
+    
+    if ( $name =~ /(.*)[*]$/ )
+    {
+	my $n = $1;
+	$user->real_name($n);
+	$user->first_name('');
+	$user->middle_name('');
+	$user->last_name($n);
+    }
+    
+    elsif ( $name =~ /(.*?) (.*) (.*)/ )
+    {
+	my $f = $1;
+	my $m = $2;
+	my $l = $3;
+	
+	$user->real_name($name);
+	$user->first_name($f);
+	$user->middle_name($m);
+	$user->last_name($l);
+    }
+    
+    elsif ( $name =~ /(.*?) (.*)/ )
+    {
+	my $f = $1;
+	my $l = $2;
+	
+	$user->real_name($name);
+	$user->first_name($f);
+	$user->middle_name('');
+	$user->last_name($l);
+    }
 }
 
 sub execute {
     my ($self, $opt, $args) = @_;
     my $users = Wing->db->resultset('User');
     if ($opt->{add}) {
-        eval { add_user($users, $opt->{add}, $opt->{password}, 
-			$opt->{admin}, $opt->{email}, $opt->{role}) };
+        eval { add_user($users, $opt->{add}, $opt->{password}, $opt->{admin}, 
+			$opt->{email}, $opt->{role}, $opt->{name}) };
         say($@ ? bleep : $opt->{add}. ' created'); 
     }
     elsif ($opt->{modify}) {
         eval { modify_user($users, $opt->{modify}, $opt->{username}, $opt->{password}, 
-			   $opt->{admin}, $opt->{email}, $opt->{role}, $opt->{status}) };
+			   $opt->{admin}, $opt->{email}, $opt->{role}, $opt->{status}, 
+			   $opt->{name}) };
         say($@ ? bleep : $opt->{modify}. ' updated'); 
     }
     elsif ($opt->{search}) {
